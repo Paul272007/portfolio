aa9ae421f8473584b46a1151cf88f4d33b31e958
Paul Maillard <pmaillard27@proton.me>
2026-05-16 13:32:46 +0200

    test nerd fonts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
+  </p>
+  <p>
+    I really <i class="nf nf-fa-heart"></i> <i class="nf nf-custom-vim"></i>
   </p>
 </template>
 
